6372a6e9bec171bd24ee09f111b26b39ec43bea7
pracsol <theshephard@hotmail.com>
2025-04-08 10:21:39 -0800

    Added use of the SkipLocalsInit attribute to prevent the runtime from automatically zeroing stack-allocated variables. Replaced memorymarshal.cast with memorymarshal.read to skip the cast operation for performance and safety. Added an overload for the Deserialize method to take a span instead of a byte array when available. Broke the "slow path" deserialization process (using unmanaged memory) out into its own method for better JIT optimization with code paths (since this is an edge case scenario it's not the frequency code path - or shouldn't be, right?). Changed how the memory zeroing is done in the "slow path" deserializer to avoid the creation of a new byte array, now just does it directly. Replaced possible nullreferenceexception in deserialize method with an explicity null check of the the data argument. All tests pass. Seems to be some promising improvements in test time execution, but will need to profile more often. My profiler on laptop is at best getting 55.5 enqueues / sec (single thread, no change compared to premod) and 13.47 dequeues (seems better than premod stats, where I was in single digits).

--- src/ModernDiskQueue/Implementation/MarshallHelper.cs
+++ src/ModernDiskQueue/Implementation/MarshallHelper.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Runtime.InteropServices;
-
-namespace ModernDiskQueue.Implementation
+﻿namespace ModernDiskQueue.Implementation
 {
+    using System;
+    using System.Runtime.CompilerServices;
+    using System.Runtime.InteropServices;
+
     /// <summary>
     /// Helper class used to help with serializing to bytes
     /// </summary>
@@ -13,6 +14,7 @@ namespace ModernDiskQueue.Implementation
         /// </summary>
         /// <param name="value">The structure to serialize to a byte array.</param>
         /// <returns>The object serialized to a byte array, ready to write to a stream.</returns>
+        [SkipLocalsInit]
         public static byte[] Serialize<T>(T value) where T : struct
         {
             // Get the size of our structure in bytes
@@ -30,23 +32,59 @@ namespace ModernDiskQueue.Implementation
         /// </summary>
         /// <param name="data">The object binary data to deserialize.</param>
         /// <returns>The deserialized structure.</returns>
+        /// <exception cref="ArgumentNullException">Returned when data is null.</exception>"
+        [SkipLocalsInit]
         public static T Deserialize<T>(byte[] data) where T : struct
         {
+            ArgumentNullException.ThrowIfNull(data);
+
             var structSize = Marshal.SizeOf<T>();
 
             // If we have complete data, use the fast path
             if (data.Length >= structSize)
             {
-                var span = MemoryMarshal.Cast<byte, T>(data.AsSpan());
-                return span[0];
+                return MemoryMarshal.Read<T>(data);
             }
 
             // Slow path for partial data
+            return DeserializePartial<T>(data, structSize);
+        }
+
+        /// <summary>
+        /// Deserializes a structure from a ReadOnlySpan - faster alternative when data is already in a span.
+        /// </summary>
+        /// <param name="data">The span containing binary data to deserialize.</param>
+        /// <returns>The deserialized structure.</returns>
+        [SkipLocalsInit]
+        public static T Deserialize<T>(ReadOnlySpan<byte> data) where T : struct
+        {
+            var structSize = Marshal.SizeOf<T>();
+
+            // Fast path for complete data
+            if (data.Length >= structSize)
+            {
+                return MemoryMarshal.Read<T>(data);
+            }
+
+            // Convert to array for partial data path
+            return DeserializePartial<T>(data.ToArray(), structSize);
+        }
+
+        /// <summary>
+        /// Helper for handling partial data deserialization.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static T DeserializePartial<T>(byte[] data, int structSize) where T : struct
+        {
             var pointer = IntPtr.Zero;
             try
             {
                 pointer = Marshal.AllocHGlobal(structSize);
-                Marshal.Copy(new byte[structSize], 0, pointer, structSize);
+                // Zero out the memory first
+                unsafe
+                {
+                    Unsafe.InitBlockUnaligned((void*)pointer, 0, (uint)structSize);
+                }
                 Marshal.Copy(data, 0, pointer, Math.Min(data.Length, structSize));
                 return Marshal.PtrToStructure<T>(pointer);
             }
