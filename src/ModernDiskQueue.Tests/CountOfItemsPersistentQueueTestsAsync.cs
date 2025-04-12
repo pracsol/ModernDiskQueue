a5376ebb20e5ae4da288d15e194c872a3b4c34e3
Stephen <theshephard@hotmail.com>
2025-04-11 20:07:30 -0800

    Added async test class for CountOfItemsPersistentQueueTests

---
+++ src/ModernDiskQueue.Tests/CountOfItemsPersistentQueueTestsAsync.cs
@@ -0,0 +1,60 @@
+using NUnit.Framework;
+using System.Threading;
+using System.Threading.Tasks;
+// ReSharper disable PossibleNullReferenceException
+
+namespace ModernDiskQueue.Tests
+{
+    [TestFixture, SingleThreaded]
+    public class CountOfItemsPersistentQueueTestsAsync : PersistentQueueTestsBase
+    {
+        protected override string QueuePath => "./CountOfItemsTests";
+
+        [Test]
+        public async Task Can_get_count_from_queue()
+        {
+            using (var queue = await PersistentQueue.CreateAsync(QueuePath))
+            {
+                Assert.That(0, Is.EqualTo(queue.EstimatedCountOfItemsInQueue));
+            }
+        }
+
+        [Test]
+        public async Task Can_enter_items_and_get_count_of_items()
+        {
+            using (var queue = await PersistentQueue.CreateAsync(QueuePath))
+            {
+                for (byte i = 0; i < 5; i++)
+                {
+                    using (var session = await queue.OpenSessionAsync())
+                    {
+                        await session.EnqueueAsync(new[] { i });
+                        await session.FlushAsync();
+                    }
+                }
+                Assert.That(5, Is.EqualTo(queue.EstimatedCountOfItemsInQueue));
+            }
+        }
+
+        [Test]
+        public async Task Can_get_count_of_items_after_queue_restart()
+        {
+            using (var queue = await PersistentQueue.CreateAsync(QueuePath))
+            {
+                for (byte i = 0; i < 5; i++)
+                {
+                    using (var session = await queue.OpenSessionAsync())
+                    {
+                        await session.EnqueueAsync(new[] { i });
+                        await session.FlushAsync();
+                    }
+                }
+            }
+
+            using (var queue = await PersistentQueue.CreateAsync(QueuePath))
+            {
+                Assert.That(5, Is.EqualTo(queue.EstimatedCountOfItemsInQueue));
+            }
+        }
+    }
+}
