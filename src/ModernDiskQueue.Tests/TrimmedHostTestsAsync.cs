0cd215b094ea8a9d30c8177c124c5fff1a23d231
Stephen <theshephard@hotmail.com>
2025-04-17 20:22:34 -0800

    Added async tests for trimmed consumer of library.

---
+++ src/ModernDiskQueue.Tests/TrimmedHostTestsAsync.cs
@@ -0,0 +1,142 @@
+﻿
+namespace ModernDiskQueue.Tests
+{
+    using NUnit.Framework;
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class TrimmedHostTestsAsync
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+        }
+
+        [Test]
+        public async Task TestTrimmedExecutable_ComplexTypeWithDefaultSerializer_CantDeserialize()
+        {
+            string path = "TrimmedHost/TestTrimmedExecutable.exe";
+            DateTimeOffset inputDate = DateTimeOffset.Now;
+            string argument = inputDate.ToString();
+            string stdOut, stdErr = string.Empty;
+            int processTimeOut = 3000;
+
+            //ARRANGE
+            ProcessStartInfo processStartInfo = new()
+            {
+                FileName = path,
+                Arguments = $"test=4 value={argument}",
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            };
+
+            Assert.That(File.Exists(path), Is.True, "Checking that file exists: TestTrimmedExecutable.exe.");
+
+            try
+            {
+                // ACT
+                using (var process = new Process { StartInfo = processStartInfo })
+                {
+                    process.Start();
+                    stdOut = process.StandardOutput.ReadToEnd();
+                    stdErr = process.StandardError.ReadToEnd();
+                    bool isProcessDone = process.WaitForExit(processTimeOut);
+
+                    if (!isProcessDone)
+                    {
+                        process.Kill();
+                        Assert.Fail("Executable did not complete within specified timeout.");
+                    }
+
+                    if (!string.IsNullOrEmpty(stdErr))
+                    {
+                        Assert.Pass($"Executable returned an error: {stdErr}");
+                    }
+
+                    Assert.That(stdOut, Is.Not.Empty, "Executable did not return any data.");
+                    Assert.That(DateTimeOffset.TryParse(stdOut, out DateTimeOffset returnedValue), Is.True, "Could not parse returned value.");
+                    Assert.That(inputDate, Is.EqualTo(returnedValue), "Returned value did not match input.");
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                Assert.Fail("InvalidOperationException trying to run test.");
+            }
+            catch (AssertionException) { }
+            catch (SuccessException) { }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Exception trying to run test. {ex.GetType().Name} {ex.Message} {ex.StackTrace} {stdErr}");
+            }
+        }
+
+        [Test]
+        public void TestTrimmedExecutable_SimpleTypeWithDefaultSerializer_CanDeserialize()
+        {
+            string path = "TrimmedHost/TestTrimmedExecutable.exe";
+            int argument = 5;
+            string stdOut, stdErr = string.Empty;
+            int processTimeOut = 3000;
+
+            //ARRANGE
+            var processStartInfo = new ProcessStartInfo()
+            {
+                FileName = path,
+                Arguments = $"test=3 value={argument}",
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            };
+
+            Assert.That(File.Exists(path), Is.True, "Checking that file exists: TestTrimmedExecutable.exe.");
+
+            try
+            {
+                // ACT
+                using (var process = new Process { StartInfo = processStartInfo })
+                {
+                    process.Start();
+                    stdOut = process.StandardOutput.ReadToEnd();
+                    stdErr = process.StandardError.ReadToEnd();
+                    bool isProcessDone = process.WaitForExit(processTimeOut);
+
+                    if (!isProcessDone)
+                    {
+                        process.Kill();
+                        Assert.Fail("Executable did not complete within specified timeout.");
+                    }
+
+                    if (!string.IsNullOrEmpty(stdErr))
+                    {
+                        Assert.Fail($"Executable returned an error: {stdErr}");
+                    }
+
+                    Assert.That(stdOut, Is.Not.Empty, "Executable did not return any data.");
+                    Assert.That(int.TryParse(stdOut, out int returnedValue), Is.True, "Could not parse output value from executable.");
+                    Assert.That(argument, Is.EqualTo(returnedValue), "Return value did not match input.");
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                Assert.Fail("InvalidOperationException trying to run test.");
+            }
+            catch (AssertionException) { }
+            catch (SuccessException) { }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Exception trying to run test. {ex.GetType().Name} {ex.Message} {ex.StackTrace} {stdErr}");
+            }
+        }
+    }
+}
