761dcd8fbf43c1be366a84576f7d774e9bd14362
Stephen <theshephard@hotmail.com>
2025-05-16 16:29:12 -0800

    Created abstract classes with default implementations of methods to reduce consumer friction in defining custom serialization strategies.

--- src/ModernDiskQueue/ISerializationStrategy.cs
+++ src/ModernDiskQueue/ISerializationStrategy.cs
@@ -1,9 +1,44 @@
-﻿namespace ModernDiskQueue
+﻿using System.Threading.Tasks;
+using System.Threading;
+
+namespace ModernDiskQueue
 {
     /// <summary>
     /// This class performs basic binary serialization from objects of Type T to byte arrays suitable for use in DiskQueue sessions.
     /// </summary>
-    public interface ISerializationStrategy<T> : ISyncSerializationStrategy<T>, IAsyncSerializationStrategy<T>
-    {
+    /// <remarks>
+    /// If you don't want to define both sync and async (de)serialization methods, consider using <see cref="AsyncSerializationStrategyBase{T}"/> or <see cref="SyncSerializationStrategyBase{T}"/>.
+    /// </remarks>
+    public interface ISerializationStrategy<T>
+    {        
+        /// <summary>
+        /// Asynchronously deserializes byte array into object reference of type T.
+        /// </summary>
+        /// <param name="bytes">Byte array to deserialize</param>
+        /// <param name="cancellationToken">Token to monitor for cancellation requests</param>
+        /// <returns>A task that represents the asynchronous operation with the deserialized object</returns>
+        public ValueTask<T?> DeserializeAsync(byte[]? bytes, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Asynchronously serializes passed object into byte array suitable for queuing.
+        /// </summary>
+        /// <param name="obj">Object to serialize. Class must be decorated with Serializable annotation.</param>
+        /// <param name="cancellationToken">Token to monitor for cancellation requests</param>
+        /// <returns>A task that represents the asynchronous operation with the serialized byte array</returns>
+        public ValueTask<byte[]?> SerializeAsync(T? obj, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Serialized passed object into byte array suitable for queuing into a <see cref="PersistentQueue{T}"/>.
+        /// </summary>
+        /// <param name="obj">Object to serialize. Class must be decorated with Serializable annotation.</param>
+        /// <returns>Byte array.</returns>
+        public byte[]? Serialize(T? obj);
+
+        /// <summary>
+        /// Deserializes byte array into object reference of type T.
+        /// </summary>
+        /// <param name="bytes">Byte array to deserialize</param>
+        /// <returns>Object instance of type T.</returns>
+        public T? Deserialize(byte[]? bytes);
     }
 }
---
+++ src/ModernDiskQueue/SyncSerializationStrategyBase.cs
@@ -0,0 +1,34 @@
+﻿namespace ModernDiskQueue
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// This abstract class allows implementation of <see cref="ISerializationStrategy{T}"/> without having to define the asynchronous (de)serialize methods.
+    /// </summary>
+    /// <remarks>
+    /// Default implementations of <see cref="SerializeAsync"/> and <see cref="DeserializeAsync"/> methods are provided, which call the synchronous versions of the methods wrapped in ValueTask return types.
+    /// </remarks>
+    public abstract class SyncSerializationStrategyBase<T> : ISerializationStrategy<T>
+    {
+        /// <inheritdoc/>
+        public abstract byte[]? Serialize(T? obj);
+
+        /// <inheritdoc/>
+        public abstract T? Deserialize(byte[]? bytes);
+
+        /// <inheritdoc/>
+        public virtual ValueTask<T?> DeserializeAsync(byte[]? bytes, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            return new ValueTask<T?>(Deserialize(bytes));
+        }
+
+        /// <inheritdoc/>
+        public virtual ValueTask<byte[]?> SerializeAsync(T? obj, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            return new ValueTask<byte[]?>(Serialize(obj));
+        }
+    }
+}
